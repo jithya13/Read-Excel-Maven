68a3f01ed04390dce6c25e3f3a187aafd2f9d343
jithya susan <jithya susan@lap1154>
2022-12-08 09:04:04 +0530

    committing read excel demo java file(Adding comments)

--- Read-Excel-Maven/src/test/java/ReadExcelDemo.java
+++ Read-Excel-Maven/src/test/java/ReadExcelDemo.java
@@ -8,10 +8,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcelDemo {
-
+//new project from git
 	public static void main(String[] args) throws IOException {
 	FileInputStream f= new FileInputStream(new File("D:\\Student.xlsx"));
-    try (XSSFWorkbook wb = new XSSFWorkbook(f)) {
+     (XSSFWorkbook wb = new XSSFWorkbook(f)) 
 		XSSFSheet ws= wb.getSheetAt(0);
 		for(Row row: ws)     //iteration over row using for each loop  
 		{  
@@ -21,7 +21,7 @@ public class ReadExcelDemo {
 		}
 		System.out.println();
 		}
-	}
+	
    
 }
 }
